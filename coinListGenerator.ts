cb3e4b16b55d9fb7be57c7c9e5131fee8bcbf868
Sol Vikings <emirhankurtofc@gmail.com>
2022-10-12 21:27:36 +0300

    Core V1.01
    - Added new major exchanges
    - Created coinListGenerator.ts ( It is providing valid exchanges for every currency with endpoint. Data.json is example Coin List.)
    - Some code cleaning.

---
+++ coinListGenerator.ts
@@ -0,0 +1,81 @@
+import axios from 'axios';
+import fs from "fs";
+
+import Exchanges from "./exchanges";
+import Tickers from './tickers';
+
+import { IExchangeAsset } from './types';
+
+const createEndpointAndTimeout = (tickerEndpoint: string, baseAsset: string, exchangeName: string) => {
+    var endpoint = tickerEndpoint + baseAsset;
+    var timeout=50;
+    switch (exchangeName) {
+        case "Binance":
+            endpoint += "USDT"
+            break;
+        case "Coinbase Exchange":
+            endpoint += "-USD/ticker"
+            timeout=150;
+            break;
+        case "FTX":
+            endpoint += "/USD"
+            break;
+        case "Kucoin":
+            endpoint += "-USDT"
+            break;
+        case "Gate.io":
+            endpoint += "_USDT"
+            break;
+        case "Bybit":
+            endpoint += "USDT&limit=1"
+            break;
+        case "Huobi":
+            endpoint = tickerEndpoint + baseAsset.toLowerCase() + "usdt&size=1"
+            break;
+        default:
+            break;
+    }
+    return {endpoint,timeout};
+}
+
+const generateCoinList = async () => {
+    var exchangeTasks = Exchanges.map(async (exchange) => {
+        console.time(`${exchange.name}, Done in`)
+        var tickerTasks = Tickers.map(async (ticker, index) => {
+            var {endpoint,timeout} = createEndpointAndTimeout(exchange.tickerEndpoint, ticker.baseAsset, exchange.name);
+            var exchangeAsset: IExchangeAsset = { exchange: { name: exchange.name, tickerEndpoint: endpoint } };
+            await new Promise((resolve, reject) => {
+                setTimeout(async () => {
+                    try {
+                        await axios(endpoint).then((data) => {
+                            if (data.data.status !== "error" && data.data.data !== null && data.data.result !== null) {
+                                ticker.exchanges!.push(exchangeAsset)
+                            }
+                        }).catch((error) => {
+                            if (error.response.status === 429) {
+                                console.count(`${exchange.name},${error.response.status} Error Count`);
+                            } else if (error.response.status !== 404) {
+                                console.log(ticker.baseAsset, exchange.name, error.response.status)
+                            }
+                        })
+                    } catch (error) {
+                        console.log(ticker.baseAsset, exchange.name, error)
+                    } finally {
+                        resolve("");
+                    }
+                }, timeout * index);
+            }).then(() => {
+                return new Promise((res, rej) => { res("") })
+            })
+        })
+        await Promise.all(tickerTasks).then(() => {
+            console.timeEnd(`${exchange.name}, Done in`)
+            return new Promise((res, rej) => { res("") })
+        })
+    })
+    await Promise.all(exchangeTasks).then(() => {
+        fs.writeFileSync('./data.json', JSON.stringify(Tickers, null, 2), 'utf-8');
+    })
+}
+
+generateCoinList()
--- exchanges.ts
+++ exchanges.ts
@@ -2,7 +2,12 @@ import { IExchange } from "./types";
 
 const Exchanges:IExchange[] = [
     {name:"Binance",tickerEndpoint:"https://api.binance.com/api/v3/ticker/price?symbol="},
-    {name:"Coinbase Exchange",tickerEndpoint:"https://api.exchange.coinbase.com/products/"}
+    {name:"Coinbase Exchange",tickerEndpoint:"https://api.exchange.coinbase.com/products/"},
+    {name:"FTX",tickerEndpoint:"https://ftx.com/api/markets/"},
+    {name:"Kucoin",tickerEndpoint:"https://api.kucoin.com/api/v1/market/orderbook/level1?symbol="},
+    {name:"Gate.io",tickerEndpoint:"https://data.gateapi.io/api2/1/ticker/"},
+    {name:"Bybit",tickerEndpoint:"https://api.bybit.com/public/linear/recent-trading-records?symbol="},
+    {name:"Huobi",tickerEndpoint:"https://api.huobi.pro/market/history/trade?symbol="}
 ]
 
 export default Exchanges;
