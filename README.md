e2951f31b0bfad113ba1c8674a832819bf92d2a9
Sol Vikings <emirhankurtofc@gmail.com>
2022-10-13 02:05:11 +0300

    Core V1.02
    -Basic version of the Program finished.
    -Updated Readme.md
    -Fixed some ticker problems for token which are have different pairs on different exchanges.

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# Crypto_Arbitrage_Scanner
+# Crypto Arbitrage Scanner
+Hello Trader! I developed **Arbitrage Scanner** for Major Exchanges.
 
-Example
-![image](https://user-images.githubusercontent.com/90453881/195456272-15c975d6-3273-4e8a-8863-f46a1390720c.png)
+**Arbitrage Scanner** based on Binance ( Scanning for currencies which are listed on Binance. ).
+- [x] Binance
+- [x] Coinbase
+- [x] FTX
+- [x] Kucoin
+- [x] Gate.io
+- [x] Bybit
+- [x] Huobi
+
+There are some incorrect outputs when different tokens have the same symbol.
+There are some time outs on the program because of api rate limits.
+
+***Example Output of The Crypto Arbitrage Scanner***
+![image](https://user-images.githubusercontent.com/90453881/195463501-84102613-4799-454f-8c18-c5bb7004644d.png)
+
+## Disclaimer
+***I am not a financial advisor. Do not take anything on [Here](https://github.com/EmirhanKrt/Crypto_Arbitrage_Scanner "Here") as financial advice, ever. Do your own research.***
---
+++ index.ts
@@ -0,0 +1,112 @@
+import axios from 'axios';
+import fs from "fs";
+
+import { IExchangeAsset, ITicker } from './types';
+
+import coinList from "./data.json";
+
+const PRICE_LIST: { baseAsset: string; exchangePrices: ({ name: string; price: number | undefined; priceDiff: number; } | undefined)[]; }[]=[];
+
+const LOWER_ARBITRAGE_FILTER=1
+
+const getPrice = (exchangeName: string,responseData:any) => {
+    let price=0;
+    switch (exchangeName) {
+        case "Binance":
+        case "Coinbase Exchange":
+            price=parseFloat(responseData.price);
+            break;
+        case "FTX":
+            price=parseFloat(responseData.result.price);
+            break;
+        case "Bybit":
+            price=parseFloat(responseData.result[0].price);
+            break;
+        case "Kucoin":
+            price=parseFloat(responseData.data.price);
+            break;
+        case "Huobi":
+            price=parseFloat(responseData.data[0].data[0].price);
+            break;
+        case "Gate.io":
+            price=parseFloat(responseData.highestBid);
+            break;
+        default:
+            break;
+    }
+    if(!price) price=0;
+    return price;
+}
+
+const getPriceRequest = (exchangeAsset:IExchangeAsset,timeout:number) => {
+    return new Promise( async (resolve, reject) => {
+        setTimeout(async() => {
+            await axios(exchangeAsset.exchange.tickerEndpoint).then((response)=>{
+                let price:number = getPrice(exchangeAsset.exchange.name,response.data);
+                exchangeAsset.price=price;
+                resolve(price)
+            }).catch((e)=>reject(e))
+        }, 100*timeout);
+    });
+}
+
+const comparePrices = async () => {
+    let tableOutput:any[]=[]
+    let priceListTableTask = PRICE_LIST.map((element)=>{
+        try {
+            if(element.exchangePrices.length<=1) return;
+            let sellHere = element.exchangePrices[element.exchangePrices.length-1];
+            if (!sellHere) sellHere = element.exchangePrices[element.exchangePrices.length-2];
+            let buyHere = element.exchangePrices[0];
+            let arbitrage = (buyHere!.priceDiff)*-1+sellHere!.priceDiff;
+            if (arbitrage > LOWER_ARBITRAGE_FILTER) tableOutput.push({baseAsset:element.baseAsset,buyHereName:buyHere!.name,buyHerePrice:buyHere!.price,sellHereName:sellHere!.name,sellHerePrice:sellHere!.price,arbitrage:arbitrage.toFixed(4)+"%"})
+            return new Promise((res,rej)=>res(""));
+            /*console.log("\n▶ "+element.baseAsset+" ◀\n"+ "▶"+buyHere!.name+" => "+buyHere!.price+" USDT\n◀"+sellHere!.name+" => "+sellHere!.price+" USDT\nArbitrage:"+ arbitrage.toFixed(4)+"%\n")*/
+        } catch (error) {
+            console.log(error)
+        }
+    })
+    await Promise.all(priceListTableTask).then(() => {
+        console.clear()
+        console.table(tableOutput)
+        tableOutput=[];
+        return new Promise((res,rej)=>{res("")})
+    })
+}
+
+const main = async () => {
+    PRICE_LIST.splice(0,PRICE_LIST.length);
+    let coinListTasks = coinList.map( async (element:ITicker,index)=>{
+        let totalPrice=0;
+        let totalExchangeCount=element.exchanges!.length;
+        var priceRequestTasks = element.exchanges!.map(async(exchangeAsset)=>{
+            await getPriceRequest(exchangeAsset,index).then((priceOfTick)=>{
+                totalPrice+=priceOfTick as number;
+                if(priceOfTick===0) totalExchangeCount--;
+                return new Promise((res, rej) => { res("") })
+            }).catch((e)=>console.log(e))
+        })
+        await Promise.all(priceRequestTasks).then(() => {
+            var avgPrice=totalPrice/totalExchangeCount;
+            element.avgPrice = avgPrice;
+            var priceCompareTasks = element.exchanges!.map((exchangeAsset)=>{
+                if (exchangeAsset.price===0) return;
+                let priceDiff = (exchangeAsset.price!/avgPrice - 1)*100;
+                return {name:exchangeAsset.exchange.name,price:exchangeAsset.price,priceDiff}
+            })
+            priceCompareTasks=priceCompareTasks.sort((a,b) => {
+                return a!.priceDiff - b!.priceDiff;
+            })
+            PRICE_LIST.push({baseAsset:element.baseAsset,exchangePrices:priceCompareTasks});
+            return new Promise((resolve,reject)=>resolve(""));
+        })
+    })
+    await Promise.all(coinListTasks).then(async() => {
+        fs.writeFileSync('./finalData.json', JSON.stringify(PRICE_LIST, null, 2), 'utf-8');
+        await comparePrices().then(()=>main())
+    })
+}
+
+main();
+
+
